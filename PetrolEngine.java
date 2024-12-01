e05aee8e889c0e4f613524a09faf8fe2de45a0a3
Akhila0210 <akhilachappidi210@gmail.com>
2024-12-01 12:17:32 -0600

    check

---
+++ PetrolEngine.java
@@ -0,0 +1,16 @@
+package CarProject;
+
+public class PetrolEngine implements IEngine {
+	public PetrolEngine() {
+		System.out.println("This is Petrol engine constructor");
+
+	}
+
+	@Override
+	public void etype() {
+		// TODO Auto-generated method stub
+		System.out.println("Petrol Engine car");
+
+	}
+
+}
